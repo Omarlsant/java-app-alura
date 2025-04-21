73f72144cd17251fdbe7b9ef6b8df7e4725f704a
Omar Lengua <omarns21@gmail.com>
2025-04-21 23:51:34 +0200

    Implement: second complementary exercise completed

--- src/PrimeraClase.java
+++ src/PrimeraClase.java
@@ -1,5 +1,8 @@
 public class PrimeraClase {
     public static void main(String[] args) {
-        System.out.println("¡Concluí la aula 01 y ahora estoy sumergiendome en Java");
+        String nombre = "Maria";
+        int edad = 30;
+        double valor = 55.9999;
+        System.out.println(String.format("Mi nombre es %s, tengo %d años y hoy gasté %.2f dolares", nombre, edad, valor));
     }
 }
